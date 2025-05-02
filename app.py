3fabf3c09408abed37dcad9b5829d4029d69a645
SohailAnsari00 <sohail.00.ansari@gmail.com>
2025-05-02 21:33:51 +0530

    updated

--- app.py
+++ app.py
@@ -1,44 +1,19 @@
-from flask import Flask, send_from_directory, abort, redirect
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='template', static_url_path='')
+app = Flask(__name__, template_folder='template', static_folder='template')
 
-# Route to serve the main HTML file
 @app.route('/')
-def serve_index():
-    return send_from_directory(app.static_folder, 'index.html')
+@app.route('/sohail')
+def home():
+    return render_template('index.html')
 
-# Route to serve manifest.json
-@app.route('/manifest.json')
-def serve_manifest():
-    return send_from_directory('.', 'manifest.json')
+@app.route('/<path:filename>')
+def static_files(filename):
+    return app.send_static_file(filename)
 
-# Route to serve serviceWorker.js
 @app.route('/serviceWorker.js')
-def serve_service_worker():
-    return send_from_directory('.', 'serviceworker.js')
-
-# Update file paths in the HTML file to correctly reference the `template/` directory
-@app.route('/src/<path:path>')
-def serve_src_files(path):
-    return send_from_directory(app.static_folder, path)
-
-# Route to serve other static files (CSS, JS, images, etc.)
-@app.route('/<path:path>')
-def serve_static(path):
-    try:
-        # Check if the file exists in the template folder
-        return send_from_directory(app.static_folder, path)
-    except FileNotFoundError:
-        # If not found in template, check the root directory
-        try:
-            return send_from_directory('.', path)
-        except FileNotFoundError:
-            abort(404)
-
-# Redirect any requests to `/None` to the home page
-@app.route('/None')
-def handle_none():
-    return redirect('/')
+def service_worker():
+    return app.send_static_file('serviceworker.js')
 
 if __name__ == '__main__':
     app.run(debug=True)
